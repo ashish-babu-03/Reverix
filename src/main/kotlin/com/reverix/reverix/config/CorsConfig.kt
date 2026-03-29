75aced7206567ba8c806d9e6fe0bb2f78f8d5ead
Moonlight <ashish.babu.sde@gmail.com>
2026-03-29 18:56:05 +0530

    Moving files to root folder

--- src/main/kotlin/com/reverix/reverix/config/CorsConfig.kt
+++ src/main/kotlin/com/reverix/reverix/config/CorsConfig.kt
@@ -13,7 +13,8 @@ class CorsConfig {
     fun corsFilter(): CorsFilter {
         val config = CorsConfiguration()
         config.allowCredentials = true
-        config.allowedOrigins = listOf("http://localhost:8080", "http://127.0.0.1:8080")
+        config.allowedOrigins = listOf("*")
+        config.allowCredentials = false
         config.allowedHeaders = listOf("*")
         config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
         config.exposedHeaders = listOf("Authorization")
